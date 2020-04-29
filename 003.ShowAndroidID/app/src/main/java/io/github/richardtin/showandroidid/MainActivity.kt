8f17e3b75deedc1e91a244d090472c9582e43beb
Richard Ting <richard.tin@gmail.com>
2020-04-29 21:43:45 +0800

    feat: show ANDROID_ID string
    ANDROID_ID is a 64-bit number (expressed as a hexadecimal string), unique to each combination of app-signing key, user, and device

--- 003.ShowAndroidID/app/src/main/java/io/github/richardtin/showandroidid/MainActivity.kt
+++ 003.ShowAndroidID/app/src/main/java/io/github/richardtin/showandroidid/MainActivity.kt
@@ -1,12 +1,17 @@
 package io.github.richardtin.showandroidid
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val deviceId: TextView = findViewById(R.id.device_id)
+        deviceId.text = Settings.Secure.getString(contentResolver, Settings.Secure.ANDROID_ID)
     }
 }
